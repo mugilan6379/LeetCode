965c53f9625f7030961cc2cc2bd9582ff58947fb
mugilan6379 <84611982+mugilan6379@users.noreply.github.com>
2025-05-27 15:04:44 -0400

    Time: 411 ms (14.29%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,16 +1,9 @@
-# Write your MySQL query statement below
--- with manager_table as (
---     select id 
---     from Employee
---     where managerId is null
--- ),
--- select distinct managerId as uniqueMID
--- from Employee
--- where managerId is not null;
-with managerIDS as (select managerId
-from Employee
-group by managerID
-having  count(managerID)>4)
+with managerIDS as (
+    select managerId
+    from Employee
+    group by managerID
+    having  count(managerID)>4
+)
 select e.name
 from Employee as e
 join managerIDS as m
